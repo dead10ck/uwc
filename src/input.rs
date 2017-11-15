acbe2e314413ff51c7c4ec8bfcb358deff75f539
Skyler Hawthorne <skyler@opendns.com>
2017-11-15 08:09:45 -0800

    Counts grapheme clusters

---
+++ src/input.rs
@@ -0,0 +1,34 @@
+use std::fs::{self, File};
+use std::io::{self, Read};
+use std::path::Path;
+
+/// The string used to identify stdin.
+pub const STDIN_IDENTIFIER : &str = "-";
+
+/// Choose between a regular file and stdin.
+pub enum Input {
+    File(fs::File),
+    Stdin(io::Stdin),
+}
+
+impl Input {
+    pub fn new<P: AsRef<Path>>(path : P) -> io::Result<Input> {
+        let path = path.as_ref();
+
+        if path.as_os_str() == STDIN_IDENTIFIER {
+            return Ok(Input::Stdin(io::stdin()));
+        }
+
+        let file = File::open(path)?;
+        Ok(Input::File(file))
+    }
+}
+
+impl Read for Input {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        match *self {
+            Input::File(ref mut file) => file.read(buf),
+            Input::Stdin(ref mut stdin) => stdin.read(buf),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,84 @@
+#[macro_use]
+extern crate log;
+
+extern crate env_logger;
+extern crate clap;
+extern crate unicode_segmentation;
+
+mod input;
+
+use std::io;
+use std::io::{BufRead, BufReader};
+
+use clap::{Arg, App, SubCommand};
+use unicode_segmentation::UnicodeSegmentation;
+
+use input::Input;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
+
+fn main() {
+    env_logger::init().unwrap();
+
+    if let Err(error) = run() {
+        eprintln!("Error: {}", error);
+        std::process::exit(1);
+    }
+}
+
+fn run() -> io::Result<()> {
+    let arg_matches = App::new("rbc")
+        .version(VERSION)
+        .author(AUTHORS)
+        .about("Counts things in text")
+        .arg(
+            Arg::with_name("grapheme-clusters")
+                .short("c")
+                .long("grapheme-clusters")
+                .help("Counts the grapheme clusters")
+                .long_help(
+                    "Counts the grapheme clusters—what you might think \
+                           of as a 'character.'",
+                ),
+        )
+        .arg(
+            Arg::with_name("FILE")
+                .help("Sets the input file to use")
+                .multiple(true)
+                .default_value("-")
+                .index(1),
+        )
+        /*
+        .subcommand(
+            SubCommand::with_name("test")
+                .about("controls testing features")
+                .version("1.3")
+                .author("Someone E. <someone_else@other.com>")
+                .arg(Arg::with_name("debug").short("d").help(
+                    "print debug information verbosely",
+                )),
+        )
+        */
+        .get_matches();
+
+    debug!("matches: {:?}", arg_matches);
+
+    // this has a default value, so it's ok to unwrap
+    let file_names: Vec<_> = arg_matches.values_of("FILE").unwrap().collect();
+
+    let mut count : u64 = 0;
+    for file_name in file_names {
+        let input = Input::new(file_name)?;
+        let reader = BufReader::new(input);
+
+        for line in reader.lines() {
+            let line = line?;
+            count += line.graphemes(true).count() as u64;
+        }
+    }
+
+    println!("grapheme clusters: {}", count);
+
+    Ok(())
+}
